4da8214b30fdd23181fde6aa12247e28ff256c21
Daniel Luna Paiva <daniel.00000853754@unicap.br>
2024-08-19 11:49:44 -0300

    serviço-pronto

---
+++ src/app/modules/home/home.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  loginCard = true;
+
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+
+  siginForm = this.formBuilder.group({
+    name: ['', Validators.required ],
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmitLoginForm(): void {
+    console.log(this.loginForm.value)
+  }
+
+  onSubmitSiginForm(): void {
+    console.log(this.siginForm.value)
+  }
+}
