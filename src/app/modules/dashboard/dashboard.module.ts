e6b6fedeacbc511adef59c513b09bd26cf4c9b6d
Daniel Luna Paiva <daniel.00000853754@unicap.br>
2024-08-21 10:26:17 -0300

    toolbar

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -5,6 +5,7 @@ import { DashboardComponent } from './page/dashboard/dashboard.component';
 
 import { SidebarModule } from 'primeng/sidebar'
 import { ToolbarModule } from 'primeng/toolbar'
+import { ChartModule } from 'primeng/chart';
 
 import { RouterModule } from '@angular/router';
 import { DASHBOARD_ROUTES } from './dashboard.routing';
@@ -12,6 +13,7 @@ import { ButtonModule } from 'primeng/button';
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
 import { CookieService } from 'ngx-cookie-service';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 
@@ -27,7 +29,9 @@ import { CookieService } from 'ngx-cookie-service';
     SidebarModule,
     ButtonModule,
     ToolbarModule,
-    ToastModule
+    ToastModule,
+    ChartModule,
+    SharedModule
   ],
   providers: [MessageService, CookieService]
 })
